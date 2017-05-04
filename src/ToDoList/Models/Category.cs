c27764e5a6e48015a02ad2a1d9fc8dbb22e28269
Jiwon Kang <jiwonk42@gmail.com>
2017-05-04 11:08:17 -0700

    add an Item Model

---
+++ src/ToDoList/Models/Category.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace ToDoList.Models
+{
+    public class Category
+    {
+        [Key]
+        public int CategoryId { get; set; }
+        public string Name { get; set; }
+        public virtual ICollection<Item> Items { get; set; }
+
+    }
+}
