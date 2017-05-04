04260cfd6b1e7ae41f258e8a50c325446e88752f
Jiwon Kang <jiwonk42@gmail.com>
2017-05-04 15:35:15 -0700

    add an Item Details page displaying each item with a corresponding Category

--- src/ToDoList/Controllers/ItemsController.cs
+++ src/ToDoList/Controllers/ItemsController.cs
@@ -4,15 +4,26 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using ToDoList.Models;
+using System.Dynamic;
+using Microsoft.EntityFrameworkCore;
 
 namespace ToDoList.Controllers
 {
     public class ItemsController : Controller
     {
         private ToDoDbContext db = new ToDoDbContext();
+
+        
         public IActionResult Index()
         {
             return View(db.Items.ToList());
+        }
+
+        public IActionResult Details(int id)
+        {
+
+            var thisItem = db.Items.Include(i => i.Category).FirstOrDefault(items => items.ItemId == id);
+            return View(thisItem);
         }
     }
 }
