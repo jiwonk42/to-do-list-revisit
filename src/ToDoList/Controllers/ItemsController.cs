2dcf9b1338406bb78ab0b57d2769b341048873ce
Jiwon Kang <jiwonk42@gmail.com>
2017-05-04 14:10:11 -0700

    add an Items Controller

---
+++ src/ToDoList/Controllers/ItemsController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ToDoList.Models;
+
+namespace ToDoList.Controllers
+{
+    public class ItemsController : Controller
+    {
+        private ToDoDbContext db = new ToDoDbContext();
+        public IActionResult Index()
+        {
+            return View(db.Items.ToList());
+        }
+    }
+}
